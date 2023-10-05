d609e542084a5d988cfb0620c3cbcb27db15cfcc
Raga <ragawilliam570@gmail.com>
2023-10-05 14:08:01 +0300

    Initialize

---
+++ src/test/java/com/example/springfiles/SpringFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
